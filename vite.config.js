9f2407de4be8dbd380c23370284ade79a58a42a4
plai020 <115868318+plai020@users.noreply.github.com>
2026-05-07 13:08:04 +0800

    v 1.1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+// 這裡的 'egg-price-tracker' 請改成你 GitHub 儲存庫（Repository）的名稱
+  base: '/egg/', 
 })
